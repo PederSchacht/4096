c3d84c148874bdab5dba419196f0a7cca0bf0712
Peder Schacht <peteschacht@gmail.com>
2014-03-27 22:41:42 -0500

    tile now can move functionality for tile combining and doubling almost working

--- app/static/js/source/4096.js
+++ app/static/js/source/4096.js
@@ -6,8 +6,8 @@ $(function(){
 
   var tileAnimation = [];
 
-  // TIle Animations:
-  tileAnimation['2']    = new $.gQ.Animation({imageURL: '../../img/num-tile-2.png'});
+  //Tile Animations:
+  tileAnimation['2']    = new $.gQ.Animation({imageURL: '../../img/num-tile-2.png', aniName:'2'});
   tileAnimation['4']    = new $.gQ.Animation({imageURL: '../../img/num-tile-4.png'});
   tileAnimation['8']    = new $.gQ.Animation({imageURL: '../../img/num-tile-8.png'});
   tileAnimation['16']   = new $.gQ.Animation({imageURL: '../../img/num-tile-16.png'});
@@ -20,12 +20,14 @@ $(function(){
   tileAnimation['2048'] = new $.gQ.Animation({imageURL: '../../img/num-tile-2048.png'});
   tileAnimation['4096'] = new $.gQ.Animation({imageURL: '../../img/num-tile-4096.png'});
 
+  var pos = ['10','130','250','370'];
+
   //Init game
   $('#playground').playground({height: PLAYGROUND_SIZE, width: PLAYGROUND_SIZE, keyTracker: true});
 
   $.playground().addGroup('tiles',{height: PLAYGROUND_SIZE, width: PLAYGROUND_SIZE});
 
-  $('#tiles').addSprite('tile', {animation: tileAnimation['2'], height: TILE_SIZE, width: TILE_SIZE});
+  $('#tiles').addSprite('tile', {animation: tileAnimation['2'], height: TILE_SIZE, width: TILE_SIZE, posx: pos[3], posy: pos[1]});
 
   //Game start
   $(document).ready(gameStart);
@@ -33,25 +35,117 @@ $(function(){
     $.playground().startGame();
   }
 
-  //Keybinding
-  $(document).keydown = function(e){
-    switch (e.keyCode){
-      case 37:
-        left();
-        $('#tile').setAnimation(tileAnimation['4']);
+  //Tile Double
+  function tileUp(tile, num){
+    console.log(tile);
+    var block = tile;
+    var n = num;
+    switch(n){
+      case 2048:
+        $('#"+tile+"').setAnimation(tileAnimation['4096']);
+        youWon();
         break;
-      case 38:
-        up();
+      case 1024:
+        tile.setAnimation(tileAnimation['2048']);
+        break;
+      case 512:
+        tile.setAnimation(tileAnimation['1024']);
+        break;
+      case 256:
+        tile.setAnimation(tileAnimation['512']);
+        break;
+      case 128:
+        tile.setAnimation(tileAnimation['256']);
+        break;
+      case 64:
+        tile.setAnimation(tileAnimation['128']);
+        break;
+      case 32:
+        tile.setAnimation(tileAnimation['64']);
+        break;
+      case 16:
+        tile.setAnimation(tileAnimation['32']);
+        break;
+      case 8:
+        tile.setAnimation(tileAnimation['16']);
+        break;
+      case 4:
+        tile.setAnimation(tileAnimation['8']);
+        break;
+      case 2:
+        console.log('working');
+        $('"#'+block+'"').setAnimation(tileAnimation['4']);
         break;
     }
-  };
+  }
+
+  function youWon(){
+    alert('You Won The Game!');
+  }
+
+  //Keybinding
+  $(document).keydown(function(e){
+    switch(e.keyCode){
+      case 37: // Left
+      case 72: // Vim Left
+      case 65: // A (WASD Left)
+        left();
+        break;
+      case 38: // Up
+      case 75: // Vim Up
+      case 87: // W (WASD Up)
+        up();
+        break;
+      case 39: // Right
+      case 76: // Vim Right
+      case 68: // D (WASD Right)
+        right();
+        break;
+      case 40: // Down
+      case 74: // Vim Down
+      case 83: // S (WASD Down)
+        down();
+        break;
+      case 32: // Space
+        space();
+        break;
+    }
+  });
+
+  function space(){
+    var $test = $('#tile');
+    var tileselect = $test.selector;
+    var testurl= $test[0].gameQuery.animation.imageURL;
+    var test2 = testurl.slice(19,-4);
+    tileUp(tileselect, test2);
+  }
 
   function left(){
-    alert('left arrow');
+    var current = parseInt($('#tile').x());
+    if(current-120 > 0){
+      $('#tile').x(current-120,false);
+    }
   }
 
   function up(){
-    alert('up arrow');
+    var current = parseInt($('#tile').y());
+    if(current-120 > 0){
+      $('#tile').y(current-120,false);
+    }
+  }
+
+  function right(){
+    var current = parseInt($('#tile').x());
+    if(current+120 < PLAYGROUND_SIZE-TILE_SIZE){
+      $('#tile').x(current+120,false);
+    }
+  }
+
+  function down(){
+    var current = parseInt($('#tile').y());
+    if(current+120 < PLAYGROUND_SIZE-TILE_SIZE){
+      $('#tile').y(current+120,false);
+    }
   }
 
 });
